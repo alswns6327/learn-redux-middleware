c375196f8a2fd2883eae710d42711f651d019dc2
alswns6327 <alswns6327@naver.com>
2023-11-25 21:20:56 +0900

    middle ware and using thunk

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
+import {createLogger} from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 root.render(
   <Provider store={store}>
